7ade1b6f0500e5a523d3173dacd6010d3643f696
Leomar Nogueira <leomar.nogueira-extern@renault.com>
2023-02-02 21:40:52 -0300

    Deve retornar o status 201 e os dados da transferência, As transações equivalentes devem ter sido geradas, A transação de saída deve ser negativa, A transação de entrada deve ser positiva, Ambas devem referenciar a transferência que as originou, Ambas devem estar com status de realizadas

--- test/routes/transfers.spec.js
+++ test/routes/transfers.spec.js
@@ -48,7 +48,7 @@ it('Deve inserir uma transferência com sucesso', async () => {
 });
 
 describe('Ao salvar uma transferência válida...', () => {
-  let transferId;
+  let transferId, output, input;
 
   it('Deve retornar o status 201 e os dados da transferência', async () => {
     const received = await request(app)
@@ -66,5 +66,40 @@ describe('Ao salvar uma transferência válida...', () => {
 
     expect(received.status).toBe(201);
     expect(received.body.description).toBe('Regular Transfer');
+  });
+
+  it('As transações equivalentes devem ter sido geradas', async () => {
+    const transactions = await app
+      .db('transactions')
+      .where({ transfer_id: transferId })
+      .orderBy('ammount');
+
+    [output, input] = transactions;
+
+    expect(transactions).toHaveLength(2);
+  });
+
+  it('A transação de saída deve ser negativa', async () => {
+    expect(output.description).toBe('Transfer to acc #999993');
+    expect(output.type).toBe('O');
+    expect(output.ammount).toBe('-100.00');
+    expect(output.acc_id).toBe(999992);
+  });
+
+  it('A transação de entrada deve ser positiva', async () => {
+    expect(input.description).toBe('Transfer from acc #999992');
+    expect(input.type).toBe('I');
+    expect(input.ammount).toBe('100.00');
+    expect(input.acc_id).toBe(999993);
+  });
+
+  it('Ambas devem referenciar a transferência que as originou', async () => {
+    expect(output.transfer_id).toBe(transferId);
+    expect(input.transfer_id).toBe(transferId);
+  });
+
+  it('Ambas devem estar com status de realizadas', async () => {
+    expect(output.status).toBe(true);
+    expect(input.status).toBe(true);
   });
 });
